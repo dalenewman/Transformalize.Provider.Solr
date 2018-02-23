17ec92ef063ede03d88f1c121d5ea00dd8bc8bbc
Dale Newman <dalenewman@gmail.com>
2018-02-22 21:48:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+### Overview
+
+This adds a `Solr` provider to Transformalize using [SolrNet](https://github.com/SolrNet/SolrNet).  It is a plug-in compatible with Transformalize 0.3.3-beta.
+
+Build the Autofac project and put it's output into Transformalize's *plugins* folder.
+
+### Write Usage
+
+```xml
+<add name='TestProcess' mode='init'>
+  <connections>
+    <add name='input' provider='bogus' seed='1' />
+    <add name='output' provider='solr' core='bogus' folder='c:\java\solr-6.2.1\cores' path='solr' port='8983' />
+  </connections>
+  <entities>
+    <add name='Contact' size='1000'>
+      <fields>
+        <add name='FirstName' />
+        <add name='LastName' />
+        <add name='Stars' type='byte' min='1' max='5' />
+        <add name='Reviewers' type='int' min='0' max='500' />
+      </fields>
+    </add>
+  </entities>
+</add>
+```
+
+This writes 1000 rows of bogus data to a Solr 6 core at *c:\java\solr-6.2.1\cores\bogus*.
+
+### Read Usage
+
+```xml
+<add name='TestProcess' >
+  <connections>
+    <add name='input' provider='solr' core='bogus' folder='c:\java\solr-6.2.1\cores' path='solr' port='8983' />
+  </connections>
+  <entities>
+    <add name='Contact' page='1' size='10'>
+      <fields>
+        <add name='firstname' />
+        <add name='lastname' />
+        <add name='stars' type='byte' />
+        <add name='reviewers' type='int' />
+      </fields>
+    </add>
+  </entities>
+</add>
+```
+
+This reads 10 rows of bogus data from a Solr 6 core at *c:\java\solr-6.2.1\cores\bogus*:
+
+<pre>
+<strong>firstname,lastname,stars,reviewers</strong>
+Justin,Konopelski,3,153
+Eula,Schinner,2,41
+Tanya,Shanahan,4,412
+Emilio,Hand,4,469
+Rachel,Abshire,3,341
+Doyle,Beatty,4,458
+Delbert,Durgan,2,174
+Harold,Blanda,4,125
+Willie,Heaney,5,342
+Sophie,Hand,2,176</pre>
+
+### Notes
+
+- Tested with Solr 6.
+- Field names go into Solr as lower case.
+- Uses older SolrNet at *https://ci.appveyor.com/nuget/solrnet-022x5w7kmuba*.
---
+++ src/Transformalize.Provider.Solr/SolrWriter.cs
@@ -0,0 +1,71 @@
+﻿#region license
+// Transformalize
+// Configurable Extract, Transform, and Load
+// Copyright 2013-2017 Dale Newman
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   
+//       http://www.apache.org/licenses/LICENSE-2.0
+//   
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+using System.Collections.Generic;
+using System.Linq;
+using SolrNet;
+using Transformalize.Configuration;
+using Transformalize.Context;
+using Transformalize.Contracts;
+using Transformalize.Extensions;
+
+namespace Transformalize.Providers.Solr {
+
+    public class SolrWriter : IWrite {
+
+        private readonly OutputContext _context;
+        readonly ISolrOperations<Dictionary<string, object>> _solr;
+        private readonly Field[] _fields;
+
+        public SolrWriter(OutputContext context, ISolrOperations<Dictionary<string, object>> solr) {
+            _context = context;
+            _solr = solr;
+            _fields = context.OutputFields.Where(f => f.Type != "byte[]").ToArray();
+        }
+
+        public void Write(IEnumerable<IRow> rows) {
+            var fullCount = 0;
+
+
+            foreach (var part in rows.Partition(_context.Entity.InsertSize)) {
+                var batchCount = (uint)0;
+                var docs = new List<Dictionary<string, object>>();
+                foreach (var row in part) {
+                    batchCount++;
+                    fullCount++;
+                    docs.Add(_fields.ToDictionary(field => field.Alias.ToLower(), field => row[field]));
+                }
+                var response = _solr.AddRange(docs);
+
+                if (response.Status == 0) {
+                    var count = batchCount;
+                    _context.Debug(() => $"{count} to output");
+                } else {
+                    _context.Error("ah!");
+                }
+            }
+
+            _solr.Commit();
+            
+            if (fullCount <= 0)
+                return;
+
+            _context.Entity.Inserts += System.Convert.ToUInt32(fullCount);
+            _context.Info($"{fullCount} to output");
+        }
+    }
+}
